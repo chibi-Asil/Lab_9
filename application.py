5b66183b1749c9ef6c4337bf88eaf7527bf484f2
chibi-Asil <32022127+chibi-Asil@users.noreply.github.com>
2020-11-02 23:45:56 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,9 @@
+# Lab 9: Birthday
+# https://cs50.harvard.edu/extension/2020/fall/labs/9/
 import os
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-# Lab 9: Birthdays
-# https://cs50.harvard.edu/extension/2020/fall/labs/9/
-# Create a web application to keep track of friends’ birthdays.
-
 
 # Configure application
 app = Flask(__name__)
@@ -21,19 +19,15 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        # When the / route is requested via GET, your web application should display, in a table, all of the people in your database along with their birthdays.
-        # First, in application.py, add logic in your GET request handling to query the birthdays.db database for all birthdays. Pass all of that data to your index.html template.
-        session["name"] = request.form.get("name")
-        session["birthday"] = request.form.get("birthday")
-        return render_template("index.html", name = request.form.get("name", "birthday"))
-            birthdays = db.execute("SELECT name, month, day FROM birthdays;")
-    return redirect("/")
+        name = name.query.filter_by(name = name).first
+        month = month.query.filter_by(month = month).numeric
+        day = day.query.filter_by(day = day).numeric
+        return redirect("/", name = name, month = month, day = day)
 
     else:
 
         # TODO: Display the entries in the database on index.html
-        # Then, in application.py, add logic in your POST request handling to INSERT a new row into the birthdays table based on the data supplied by the user.
-        birthdays = []
-    return render_template("index.html", birthdays = birthdays)
+        if not request.form.get("name") or request.form.get("month") or request.form.get("day"):
+            return render_template("index.html")
 
 
